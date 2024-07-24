da225becd376c60193c82414caab51a7a8eddd74
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-07-24 16:19:30 -0700

    task: trying out the advanced task

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Build a caching system, use the LRU cache algorithm
+"""
+from collections import OrderedDict
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    Using the LRU cache algorithm, build a caching system
+    """
+    def __init__(self):
+        """
+        Initialise the cache
+        """
+        super().__init__()
+        self.cache_indexes = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Adds an item to the cache. uses the LRU caching algo
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_indexes.move_to_end(key)
+                self.cache_data[key] = item
+                return
+
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                item_removed = self.cache_indexes.popitem(last=False)[0]
+                del self.cache_data[item_removed]
+                print(f"DISCARD: {item_removed}")
+
+            self.cache_data[key] = item
+            self.cache_indexes[key] = item
+
+    def get(self, key):
+        """
+        Get the value from the cache
+        """
+        if key in self.cache_data:
+            self.cache_indexes.move_to_end(key)
+            return self.cache_data[key]
+        return None
